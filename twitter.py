f93bab67815e0dd78cd48319fe00f894aa4ff924
J <jordkam13@gmail.com>
2021-10-17 17:27:26 +0800

    create jbTweets table

--- twitter.py
+++ twitter.py
@@ -30,9 +30,11 @@ tweets = api.user_timeline(screen_name=user_name,
 db_file = r'twitter.db'
 db = sqlite3.connect(db_file)
 c = db.cursor()
+db.set_trace_callback(print)
 
 
 # create table to store profile if not exists
+# c.execute("DROP TABLE profile")
 c.execute('''CREATE TABLE IF NOT EXISTS profile (
             screen_name VARCHAR(40),
             id INTEGER,
@@ -47,17 +49,37 @@ c.execute('''CREATE TABLE IF NOT EXISTS profile (
             );''')
 
 
-# store Joe Biden's information into profile
-c.execute('''INSERT INTO profile
-            (screen_name, id, name, location,
-            url, description, followers_count,
-            friends_count, statuses_count,
-            created_at)
-            Values (?, ?, ?, ?, ?, ?,
-            ?, ?, ?, ?);''', (user.screen_name,
-            user.id, user.name, user.location, user.url,
-            user.description, user.followers_count,
-            user.friends_count, user.statuses_count, user.created_at))
+# store Joe Biden's information into profile:
+
+joe_biden = c.execute("SELECT * FROM profile WHERE id = :id;", {'id': user.id})
+if len(c.fetchall()) == 0:     # if joe biden record not exists
+
+    c.execute('''INSERT INTO profile
+                (screen_name, id, name, location,
+                url, description, followers_count,
+                friends_count, statuses_count,
+                created_at)
+                Values (?, ?, ?, ?, ?, ?,
+                ?, ?, ?, ?);''', (user.screen_name,
+                user.id, user.name, user.location, user.url,
+                user.description, user.followers_count,
+                user.friends_count, user.statuses_count, user.created_at))
+
+
+
+# create table jbTweets to store all tweets from Joe Biden
+c.execute('''CREATE TABLE IF NOT EXISTS jbTweets(
+             TweetsID INT,
+             CreatedAT DATE,
+             FullText TEXT,
+             Likes INT,
+             Retweets INT,
+             Comments INT
+             );''')
+
+
+# store 
+
 
 # save all the above changes to db
 db.commit()
