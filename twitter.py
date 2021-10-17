e7f9bb0d1b80c33aae1b75e48aa705230e2917e0
J <jordkam13@gmail.com>
2021-10-17 16:03:04 +0800

    updated twitter.py & db

--- twitter.py
+++ twitter.py
@@ -1,26 +1,53 @@
 import tweepy
 import sqlite3
 
-'''
+# keys to authenticate
 consumer_key = 'N6gSTYL6SNTNqcu0uhcW2qEue'
 consumer_secret = 'vfxvNvk8mvdH1GcFSAmOar5E2RXaiHnB0HGQ8CSg0eIu2mtQBG'
 access_token = '2769979454-VzVkbDRQRjCxjNCuipQ2yaAcSIt6XzdSSaVH2B7'
 access_token_secret = 'JtezsQUiV9EDf1ehPQm4G2i03mQbXi5637pi1QsBJCEWP'
-
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 
+# initialize API via Tweepy
 api = tweepy.API(auth)
 user_name = 'JoeBiden'
-user = api.get_user(screen_name=user_name)
-print(user.followers_count)
-'''
 
-db_file = r'test.db'
+
+# get Joe Biden's user object
+user = api.get_user(screen_name=user_name)
+
+
+# get Joe Biden's tweets 
+tweets = api.user_timeline(screen_name=user_name, 
+                           count=1,
+                           include_rts = False,
+                           tweet_mode = 'extended')
+
+
+# initialize db
+db_file = r'twitter.db'
 db = sqlite3.connect(db_file)
-rows = db.execute("SELECT * FROM team_mate;")
-for row in rows:
-    print(row)
+c = db.cursor()
+
+
+# create table to store profile if not exists
+c.execute('''CREATE TABLE IF NOT EXISTS profile (
+            screen_name VARCHAR(40),
+            id INTEGER,
+            name VARCHAR(40),
+            location VARCHAR(20),
+            url VARCHAR(50),
+            description TEXT,
+            followers_count INTEGER,
+            friends_count INTEGER,
+            statuses_count INTEGER,
+            creted_at VARCHAR(30)
+            );''')
+
+
+
+db.commit()
 
 db.close()
